0d95faeec8c6efc49d40c7e239b8e52ba4644e6c
4elovel <marcynakvova007@gmail.com>
2023-10-18 17:34:44 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        static void ToDo()
+        {
+            List<string> list = new List<string> { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f" };
+            foreach (string s in list)
+            {
+                Console.Write($"{s} ");
+            }
+        }
+        Thread thread = new Thread(ToDo);
+        thread.Start();
+    }
+}
